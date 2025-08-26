03cae3a1535e6e8cfe6709355c0b4e6e5e4ef4bc
Anant Jindal <anant.jindal@kredmint.com>
2025-08-26 21:13:46 +0530

    Written a logic for stone paper scissor

---
+++ rock_game.py
@@ -0,0 +1,36 @@
+import random
+
+def get_choice():
+    player_choice = input("Enter a choice from rock, paper, scissors:")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+
+values = get_choice()
+
+def get_winner(player, computer):
+    print(f"You chossen {player}, computer chooses {computer}")
+
+    if player == 'rock' and computer == 'scissors':
+        return "It's a tie!"
+    elif player == 'rock':
+        if computer == "paper":
+            return "Paper covers rock, you lose"
+        else:
+            return "Rock smasshes scissors, you win!"
+    elif player == 'paper':
+        if computer == "rock":
+            return "Paper covers rock, you win"
+        else:
+            return  "Rock smasshes paper, you lose!"
+    elif player == 'sccisor':
+        if computer == "rock":
+            return "Rock smasshes pscissor you lose"
+        else:
+            return  "Scissor cuts paper, you win!"
+        
+
+    win = get_winner(values["player"], values["computer"])
+    print(win)
