f8a99eb18431ce59683f6e9cd541a35aa192127d
Anant Jindal <anant.jindal@kredmint.com>
2025-08-29 20:58:17 +0530

    fixed machine choice

--- random_number.py
+++ random_number.py
@@ -1,7 +1,5 @@
 import random;
 
-machine_num = random.randrange(0, 11);
-
 user_wants = input("Do you want to play a number choice game? (Yes/No): ")
 
 if user_wants.lower() != 'yes':
@@ -12,6 +10,7 @@ how_many_games =  input("How many games you want to play? ");
 game_played = 0
 
 while game_played<int(how_many_games):
+    machine_num = random.randrange(0, 11);
     user_num = input("Choose a number between 0 to 10: ");
     game_played += 1;
     if int(user_num) == machine_num:
